46b61b00926b240fe96521765618a61448d2b7c6
osamahamada2024 <hmsosamagithub773@gmail.com>
2025-12-20 23:57:41 +0200

    refactor: Rename imported symbols in `lava` module to PascalCase.

--- environments/lava/__init__.py
+++ environments/lava/__init__.py
@@ -3,21 +3,21 @@
 Lava Maze Module
 """
 
-from .heat_haze_fog import heat_haze_fog
-from .lava_maze_scene import lava_maze_scene
-from .lava_audio_system import lava_audio_system
-from .lava_zone import lava_zone, LavaZoneManager
-from .fire_particle_system import Ember, fire_particle_system
+from .heat_haze_fog import HeatHazeFog
+from .lava_maze_scene import LavaMazeScene
+from .lava_audio_system import LavaAudioSystem
+from .lava_zone import LavaZone, LavaZoneManager
+from .fire_particle_system import Ember, FireParticleSystem
 from .volcanic_environment import VolcanicEnvironmentManager, VolcanicRock
 
 __all__ = [
-    'heat_haze_fog',
-    'lava_maze_scene', 
-    'lava_audio_system',
-    'lava_zone',
+    'HeatHazeFog',
+    'LavaMazeScene', 
+    'LavaAudioSystem',
+    'LavaZone',
     'LavaZoneManager',
     'Ember',
-    'fire_particle_system',
+    'FireParticleSystem',
     'VolcanicEnvironmentManager',
     'VolcanicRock'
 ]

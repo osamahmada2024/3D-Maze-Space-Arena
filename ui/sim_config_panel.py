70a7f23813ee765443d6fb91c275edb8d1d56895
osamahamada2024 <hmsosamagithub773@gmail.com>
2025-12-21 02:54:14 +0200

    Rename "Greedy" algorithm to "Hill Climbing" in simulation configuration UI.

--- ui/sim_config_panel.py
+++ ui/sim_config_panel.py
@@ -163,7 +163,7 @@ class SimConfigPanel:
         }
         
         # Shortened algorithm names for UI
-        self.algorithms = ["A*", "BFS", "DFS", "Dijkstra", "IDS", "Greedy", "Genetic", "Beam", "Bi-Dir"]
+        self.algorithms = ["A*", "BFS", "DFS", "Dijkstra", "IDS", "Hill Climbing", "Genetic", "Beam", "Bi-Dir"]
         self.shapes = ["Sphere", "Cube", "Drone", "Alien"]
         self.dist_options = ["Near", "Mid", "Far"]
         
@@ -451,7 +451,7 @@ class SimConfigPanel:
             "DFS": "DFS",
             "Dijkstra": "Dijkstra",
             "IDS": "IDS",
-            "Greedy": "Hill Climbing",
+            "Hill Climbing": "Hill Climbing",
             "Genetic": "Genetic",
             "Beam": "Beam Search",
             "Bi-Dir": "Bidirectional"
